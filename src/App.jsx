f1a8bf9787ce6e7eb7c12bd4cfe9e78f8f3c2fb8
Santho257 <santhoshkumar02553@gmail.com>
2024-02-20 14:55:23 +0530

    Port Number Changes

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
 
   const fetchData = async (tabName, func) => {
     try {
-      let result = await axios(`http://localhost:2507/${tabName}`);
+      let result = await axios(`http://localhost:3001/${tabName}`);
       func(result.data);
     }
     catch (err) {
